6559842acdb6a6f1ff42a26bdc08d94e8233fde4
burrito-mode <jpantsniner@gmail.com>
2021-10-25 14:50:36 -0600

    create Session model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy import  CheckConstraint
+from datetime import datetime
 
 
 
@@ -9,6 +11,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    sessions = db.relationship('Session', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -23,3 +26,18 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+
+class Session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    putt_distance = db.Column(db.Integer)
+    number_attempts = db.Column(db.Integer)
+    number_putts_made = db.Column(db.Integer)
+    CheckConstraint(number_putts_made <= number_attempts)
+
+    def __repr__(self):
+        return '<Session {}: {} putts out of {} made from {} feet>'.format(self.id, self.number_putts_made,
+                                                                           self.number_attempts, self.putt_distance)
+
