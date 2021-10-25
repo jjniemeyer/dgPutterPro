d2cd7250d02f1edb2b16f05bd4c66faf53a4d101
burrito-mode <jpantsniner@gmail.com>
2021-10-25 15:11:38 -0600

    rename Session table to Drill table for clarity

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    sessions = db.relationship('Session', backref='user', lazy='dynamic')
+    drills = db.relationship('Drill', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -28,7 +28,7 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-class Session(db.Model):
+class Drill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
@@ -38,6 +38,6 @@ class Session(db.Model):
     CheckConstraint(number_putts_made <= number_attempts)
 
     def __repr__(self):
-        return '<Session {}: {} putts out of {} made from {} feet>'.format(self.id, self.number_putts_made,
+        return '<Drill {}: {} putts out of {} made from {} feet>'.format(self.id, self.number_putts_made,
                                                                            self.number_attempts, self.putt_distance)
 
