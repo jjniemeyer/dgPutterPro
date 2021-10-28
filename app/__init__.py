6b9fcaa95495e75d1d5c7a11687824098556be3c
burrito-mode <jpantsniner@gmail.com>
2021-10-27 23:16:37 -0600

    setup logging to file

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,9 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 
 
 app = Flask(__name__, static_url_path='/static')
@@ -15,6 +17,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 if not app.debug:
+    # email loggin
     if app.config['MAIL_SERVER']:
         auth = None
     if app.config['MAIL_USE_TLS']:
@@ -26,5 +29,17 @@ if not app.debug:
         credentials=auth, secure=secure)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
+    ## log to file
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/dgPutterPro.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('dgPutterPro startup')
+
 
 from app import routes, models, errors
