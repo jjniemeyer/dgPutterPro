391fef648f4270700796b0cea59841a64559343c
burrito-mode <jpantsniner@gmail.com>
2021-10-29 11:27:44 -0600

    typo

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_moment import Moment
+# from flask_moment import Moment
 from config import Config
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
