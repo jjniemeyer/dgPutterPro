cc736f84e0c8bb0cb126e2ccee546974fdc15aba
burrito-mode <jpantsniner@gmail.com>
2021-12-04 21:32:07 -0700

    move DATABASE_URL to environment variable

--- config.py
+++ config.py
@@ -5,9 +5,7 @@ load_dotenv(os.path.join(basedir,  '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    DATABASE_URL = 'postgresql+psycopg2://postgres:postgres@database:5432/dgputterpro'
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
